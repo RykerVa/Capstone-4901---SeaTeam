5c86e951eb37e34bcc9acdecef376c83ee1460ba
RykerVa <135276174+RykerVa@users.noreply.github.com>
2023-11-06 22:43:44 -0600

    Update api.go

--- api/api.go
+++ api/api.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	// Start the HTTP server
 	http.HandleFunc("/health", healthCheckHandler)
+	http.HandleFunc("/endpoint1", endpoint1Handler)
+	http.HandleFunc("/endpoint2", endpoint2handler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -34,6 +36,44 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Set the Content-Type header and write the response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJSON)
+}
+
+//for right now these are just basic endpoints and we can implement specific logic in the future.
+func endpoint1Handler(w http.ResponseWriter, r *http.Request){
+	
+	data := map[string]string{
+		"message": "This is Endpoint 1",
+	}
+
+	responseJSON, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	//set header and write the response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJSON)
+}
+
+//for right now these are just basic endpoints and we can implement specific logic in the future.
+func endpoint2handler(w http.ResponseWriter, r *http.Request){
+	
+	data := map[string]string{
+		"message": "This is Endpoint 2",
+	}
+
+	responseJSON, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	//set header and write the response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(responseJSON)
