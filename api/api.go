911194070c6ca3e6d92101dffd6d62ae75f6d0d5
RykerVa <135276174+RykerVa@users.noreply.github.com>
2023-11-11 21:17:26 -0600

    Add files via upload

--- api/api.go
+++ api/api.go
@@ -1,40 +1,72 @@
-package main
+// api.go
+
+package api
 
 import (
-	"encoding/json"
-	"net/http"
+    "encoding/json"
+    "net/http"
 )
 
-func main() {
-	// Start the HTTP server
-	http.HandleFunc("/health", healthCheckHandler)
-	http.ListenAndServe(":8080", nil)
+// HealthCheckHandler handles health check requests.
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    // Need to fill in more check logic once the router is more coded
+    healthStatus := "healthy"
+    message := "The proxy/router is operating normally"
+
+    // Create a response JSON
+    response := struct {
+        Status  string `json:"status"`
+        Message string `json:"message"`
+    }{
+        Status:  healthStatus,
+        Message: message,
+    }
+
+    // Marshal the response into JSON
+    responseJSON, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    // Set the Content-Type header and write the response
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(responseJSON)
 }
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	
-	//Need to fill in more check logic once the router is more coded
-	healthStatus := "healthy"
-	message := "The proxy/router is operating normally."
+// Endpoint1Handler handles requests for the first endpoint.
+func Endpoint1Handler(w http.ResponseWriter, r *http.Request) {
+    data := map[string]string{
+        "message": "This is Endpoint 1",
+    }
 
-	// Create a response JSON
-	response := struct {
-		Status  string `json:"status"`
-		Message string `json:"message"`
-	}{
-		Status:  healthStatus,
-		Message: message,
-	}
+    responseJSON, err := json.Marshal(data)
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
 
-	// Marshal the response into JSON
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+    // Set header and write the response
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(responseJSON)
+}
 
-	// Set the Content-Type header and write the response
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(responseJSON)
+// Endpoint2Handler handles requests for the second endpoint.
+func Endpoint2Handler(w http.ResponseWriter, r *http.Request) {
+    data := map[string]string{
+        "message": "This is Endpoint 2",
+    }
+
+    responseJSON, err := json.Marshal(data)
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    // Set header and write the response
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(responseJSON)
 }
