191292913d270cd427d570a5d57eb65b49c26f24
RykerVa <135276174+RykerVa@users.noreply.github.com>
2023-11-11 21:08:47 -0600

    Add files via upload

--- mockbackend/mockbackend.go
+++ mockbackend/mockbackend.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from the Root Path")
+	})
+
+	http.HandleFunc("/service1", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from Service 1")
+	})
+
+	http.HandleFunc("/service2", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from Service 2")
+	})
+
+	http.ListenAndServe(":1234", nil)
+}
