78ccd95bbf406deadc3f1bf9243f0b78382bce4a
A-Monjaras <135273644+A-Monjaras@users.noreply.github.com>
2024-04-23 16:53:21 -0500

    Added Zipkin

--- app/go.mod
+++ app/go.mod
@@ -4,18 +4,28 @@ go 1.22
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0
-	github.com/prometheus/client_golang v1.18.0
+	github.com/prometheus/client_golang v1.19.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/openzipkin/zipkin-go v0.4.2 // indirect
+	go.opentelemetry.io/otel/metric v1.25.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.25.0 // indirect
+	go.opentelemetry.io/otel/trace v1.25.0 // indirect
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	go.opentelemetry.io/otel v1.25.0
+	go.opentelemetry.io/otel/exporters/zipkin v1.25.0
+	golang.org/x/sys v0.18.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -6,17 +6,401 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	// Import the necessary OpenTelemetry packages
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 
 	api "seateam/api"
 	config "seateam/config"
 	loadbalancer "seateam/loadbalancer"
 )
 
+func traceMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tracer := otel.Tracer("myApp")
+		ctx, span := tracer.Start(r.Context(), "http.request")
+		// Set attributes after starting the span
+		span.SetAttributes(
+			attribute.String("method", r.Method),
+			attribute.String("path", r.URL.Path),
+		)
+		defer span.End()
+
+		// Continue with the request
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func initTracing() {
+	// Configure the Zipkin exporter to send traces to a Zipkin backend
+	exporter, err := zipkin.New("http://zipkin:9411/api/v2/spans")
+	if err != nil {
+		log.Fatalf("Failed to create Zipkin exporter: %v", err)
+	}
+
+	// Create a new tracer provider with a batch span processor and the Zipkin exporter
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		// Add a resource to the tracer provider to identify this application
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("SeaTeamSVC"),
+		)),
+	)
+
+	// Set the created tracer provider as the global provider
+	otel.SetTracerProvider(tp)
+}
+
+var (
+	// Counter for each endpoint
+	endpointRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "endpoint_requests_total",
+			Help: "Total number of requests to each endpoint.",
+		},
+		[]string{"endpoint"},
+	)
+
+	// Total number of endpoints
+	totalEndpoints = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "total_endpoints",
+			Help: "Total number of endpoints.",
+		},
+	)
+
+	totalRequests = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "total_requests",
+			Help: "Total number of requests across all endpoints.",
+		},
+	)
+
+	// Mutex to ensure safe access to connectionCount
+	mu sync.Mutex
+)
+
+func init() {
+	prometheus.MustRegister(endpointRequests)
+	prometheus.MustRegister(totalEndpoints)
+	prometheus.MustRegister(totalRequests)
+}
+
+func incrementTotalEndpoints(count int) {
+	mu.Lock()
+	defer mu.Unlock()
+	totalEndpoints.Set(float64(count))
+}
+
 func main() {
+
+	initTracing()
+	// Handler for the first endpoint (listening on port 1234)
+	mux1 := http.NewServeMux()
+	mux1.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpointRequests.WithLabelValues("endpoint_1").Inc()
+		totalRequests.Inc()
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Envoy Router</title>
+				<link rel="stylesheet" type="text/css" href="styles.css">
+				<style>
+					body {
+						font-family: Arial, sans-serif;
+						background-color: #f0f0f0;
+						color: #333;
+						margin: 20px;
+					}
+
+					h1 {
+						color: #3498db;
+					}
+
+					button {
+						display: block;
+						padding: 15px 40px;
+						color: #fff;
+						text-decoration: none;
+						background-color: #3498db;
+						border-radius: 8px;
+						transition: background-color 0.3s, color 0.3s;
+						border: none; /* Remove default button border */
+						cursor: pointer;
+					}
+
+					button:hover {
+						background-color: #267bb5;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Response from mockbackend 1 - Root Path</h1>
+				<button onclick="sendNewRequest()">New Request</button>
+
+				<script>
+					function sendNewRequest() {
+						// You can customize this URL based on your requirements
+						window.location.href = "/?endpoint=lb";
+					}
+				</script>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
+	mux1.HandleFunc("/service1", func(w http.ResponseWriter, r *http.Request) {
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Envoy Router</title>
+				<link rel="stylesheet" type="text/css" href="styles.css">
+			</head>
+			<body>
+				<h1>Response from mockbackend 1 - Service1</h1>
+				<button onclick="sendNewRequest()">New Request</button>
+
+				<script>
+					function sendNewRequest() {
+						// You can customize this URL based on your requirements
+						window.location.href = "/?endpoint=lb";
+					}
+				</script>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
+
+	// Start the server on port 1234
+	go func() {
+		http.ListenAndServe(":1234", mux1)
+	}()
+
+	// Handler for the second endpoint (listening on port 5678)
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpointRequests.WithLabelValues("endpoint_2").Inc()
+		totalRequests.Inc()
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Envoy Router</title>
+				<link rel="stylesheet" type="text/css" href="styles.css">
+				<style>
+					body {
+						font-family: Arial, sans-serif;
+						background-color: #f0f0f0;
+						color: #333;
+						margin: 20px;
+					}
+
+					h1 {
+						color: #3498db;
+					}
+
+					button {
+						display: block;
+						padding: 15px 40px;
+						color: #fff;
+						text-decoration: none;
+						background-color: #00FF00;
+						border-radius: 8px;
+						transition: background-color 0.3s, color 0.3s;
+						border: none; /* Remove default button border */
+						cursor: pointer;
+					}
+
+					button:hover {
+						background-color: #008000;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Response from mockbackend 2 - Root Path</h1>
+				<button onclick="sendNewRequest()">New Request</button>
+
+				<script>
+					function sendNewRequest() {
+						// You can customize this URL based on your requirements
+						window.location.href = "/?endpoint=lb";
+					}
+				</script>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
+	mux2.HandleFunc("/service2", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from mockbackend 2 - Service2")
+	})
+
+	// Start the server on port 5678
+	go func() {
+		http.ListenAndServe(":5678", mux2)
+	}()
+
+	//Handler for the third endpoint (listening on port 9876)
+	mux3 := http.NewServeMux()
+	mux3.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpointRequests.WithLabelValues("endpoint_3").Inc()
+		totalRequests.Inc()
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Envoy Router</title>
+				<link rel="stylesheet" type="text/css" href="styles.css">
+				<style>
+					body {
+						font-family: Arial, sans-serif;
+						background-color: #f0f0f0;
+						color: #333;
+						margin: 20px;
+					}
+
+					h1 {
+						color: #3498db;
+					}
+
+					button {
+						display: block;
+						padding: 15px 40px;
+						color: #fff;
+						text-decoration: none;
+						background-color: #FF0000;
+						border-radius: 8px;
+						transition: background-color 0.3s, color 0.3s;
+						border: none; /* Remove default button border */
+						cursor: pointer;
+					}
+
+					button:hover {
+						background-color: #CC0000;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Response from mockbackend 3 - Root Path</h1>
+				<button onclick="sendNewRequest()">New Request</button>
+
+				<script>
+					function sendNewRequest() {
+						// You can customize this URL based on your requirements
+						window.location.href = "/?endpoint=lb";
+					}
+				</script>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
+	mux3.HandleFunc("/service3", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from mockbackend 3 - Service3")
+	})
+
+	// Start the server on port 5678
+	go func() {
+		http.ListenAndServe(":9876", mux3)
+	}()
+
+	//Handler for the fourth endpoint (listening on port 5544)
+	mux4 := http.NewServeMux()
+	mux4.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpointRequests.WithLabelValues("endpoint_4").Inc()
+		totalRequests.Inc()
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Envoy Router</title>
+				<link rel="stylesheet" type="text/css" href="styles.css">
+				<style>
+					body {
+						font-family: Arial, sans-serif;
+						background-color: #f0f0f0;
+						color: #333;
+						margin: 20px;
+					}
+
+					h1 {
+						color: #3498db;
+					}
+
+					button {
+						display: block;
+						padding: 15px 40px;
+						color: #fff;
+						text-decoration: none;
+						background-color: #7851A9;
+						border-radius: 8px;
+						transition: background-color 0.3s, color 0.3s;
+						border: none; /* Remove default button border */
+						cursor: pointer;
+					}
+
+					button:hover {
+						background-color: #663399;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Response from mockbackend 4 - Root Path</h1>
+				<button onclick="sendNewRequest()">New Request</button>
+
+				<script>
+					function sendNewRequest() {
+						// You can customize this URL based on your requirements
+						window.location.href = "/?endpoint=lb";
+					}
+				</script>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
+	mux4.HandleFunc("/service4", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from mockbackend 4 - Service4")
+	})
+
+	// Start the server on port 5544
+	go func() {
+		http.ListenAndServe(":5544", mux4)
+	}()
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":8081", nil)
+	}()
+
+	incrementTotalEndpoints(4)
+
 	// Initial config load
 	configuration, backendServers := loadConfig()
 
@@ -71,14 +455,15 @@ func main() {
 	})
 
 	// Serve the API endpoints
-	http.Handle("/", r)
-	http.HandleFunc("/health", api.HealthCheckHandler)
-	http.HandleFunc("/endpoint1", api.Endpoint1Handler)
-	http.HandleFunc("/endpoint2", api.Endpoint2Handler)
+	// Serve the API endpoints
+	http.Handle("/", traceMiddleware(r))
+	http.Handle("/health", traceMiddleware(http.HandlerFunc(api.HealthCheckHandler)))
+	http.Handle("/endpoint1", traceMiddleware(http.HandlerFunc(api.Endpoint1Handler)))
+	http.Handle("/endpoint2", traceMiddleware(http.HandlerFunc(api.Endpoint2Handler)))
 
 	// Serve frontend files
 	fs := http.FileServer(http.Dir("./frontend"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", traceMiddleware(fs)))
 
 	log.Println("Server started on :8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
@@ -90,6 +475,7 @@ func main() {
 	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChannel
 	log.Println("Shutting down the server...")
+
 }
 
 func loadConfig() (config.StaticBootstrap, []config.BackendServer) {
